b2a7d79e4a1558fcdadbd93d9351defae06c44e3
doglasbatista <doglasbatista@gmail.com>
2022-01-18 22:02:50 -0300

    exporting venus components

--- packages/venus/src/main.ts
+++ packages/venus/src/main.ts
@@ -1,1 +1,3 @@
 export { default as Button } from './components/Button.vue';
+export { default as Text } from './components/Text.vue';
+export { default as EmptyState } from './components/EmptyState.vue';
