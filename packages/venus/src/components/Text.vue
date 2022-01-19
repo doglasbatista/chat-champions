cb1bc5ee3b158e1ed43cb7aa1d0cc40d3a2db508
doglasbatista <doglasbatista@gmail.com>
2022-01-18 22:01:49 -0300

    create text component

---
+++ packages/venus/src/components/Text.vue
@@ -0,0 +1,31 @@
+<template>
+  <span :class="size"><slot /></span>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+
+export default defineComponent({
+  props: {
+    size: {
+      required: true,
+      type: Object as PropType<'large' | 'normal'>,
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+span {
+  color: #080808;
+  font-size: 16px;
+}
+
+.large {
+  font-size: 36px;
+}
+
+.normal {
+  font-size: 20px;
+}
+</style>
